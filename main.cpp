dca7308ef2529b24cfac1ec515ed7c19ebc89eac
Kitz2024 <kit.pollinger@gmail.com>
2024-10-05 23:05:41 -0700

    Lab-18New Commit

--- main.cpp
+++ main.cpp
@@ -12,11 +12,44 @@ struct Node
     Node *next;
 };
 
-Node *head = nullptr;
-Node *tail = nullptr;
+Node* head = nullptr;
+Node* tail = nullptr;
 
 void addNodeToTail(double ratings, string comments)
 {
+    Node* newNode=new Node;
+    newNode->ratings = ratings;
+    newNode->comments = comments;
+    newNode->next = nullptr;
+
+    if (tail == nullptr) {
+        head = tail = newNode;
+    }else {
+        tail->next = newNode;
+        tail = newNode;
+    }
+}
+
+void addNodeToHead(double ratings, string comments){
+    Node* newNode = new Node;
+    newNode->ratings = ratings;
+    newNode->comments = comments;
+    newNode->next = head; 
+    head = newNode;
+}
+
+void printReview(){
+    Node* current = head;
+    int count = 0;
+    double totalRatings = 0.0;
+
+    while (current != nullptr) {
+        cout << "> Review #" << ++count << ": " << current->ratings << ": " << current->comments << endl;
+        totalRatings += current->ratings;
+        current = current->next;
+    }
+    double averageRatings = totalRatings / count;
+    cout << " > Average: " << averageRatings << endl;
 }
 
 int main()
@@ -24,12 +57,33 @@ int main()
     int userChoice;
     double ratings;
     string comments;
-    char anotherReview;
 
-    cout << "Which linked list method should we use?";
+    cout << "Which linked list method should we use?" << endl;
     cout << "[1] New nodes are added at the head of the linked list\n";
     cout << "[2] New nodes are added at the tail of the linked list\n";
     cin >> userChoice;
+
+    for (char nextReview = 'y'; nextReview == 'y' || nextReview == 'Y';) {
+        cout << "Enter review rating 0-5: ";
+        cin >> ratings;
+        cout << "Enter review comments: ";
+        cin.ignore();
+        getline (cin, comments);
+
+        if (userChoice == 1){
+            addNodeToHead(ratings, comments);
+        }else {
+            addNodeToTail(ratings, comments);
+        }
+
+        cout << "Enter another review? Y/N: ";
+        cin >> nextReview;
+    }
+
+    cout << "Outputting all reviews:" << endl;
+    printReview();
+
+
 
     return 0;
 }
